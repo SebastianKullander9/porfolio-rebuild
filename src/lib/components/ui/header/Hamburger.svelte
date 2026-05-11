868ca0f4647140e811fa73cfa8aaccf0a947aa09
SebastianKullander9 <sebastian.kullander@protonmail.com>
2026-05-11 19:09:24 +0200

    migrate header, remove gsap for css transitions and svelte reactivity

---
+++ src/lib/components/ui/header/Hamburger.svelte
@@ -0,0 +1,32 @@
+<script>
+	import "./Hamburger.css";
+
+	let menuOpen = $state(false);
+	let isAnimating = false;
+
+	function handleClick() {
+		if (isAnimating) return;
+		menuOpen = !menuOpen;
+	}
+</script>
+
+<button
+	type="button"
+	class="group z-10 cursor-pointer"
+	onclick={handleClick}
+	aria-label="Open page navigation"
+>
+	<div class="menu-icon">
+		<div class="menu-icon-cross absolute inset-0" class:open={menuOpen}>
+			<div
+				class="absolute top-1/2 left-1/2 h-5 w-5 -translate-x-1/2 -translate-y-1/2 transform duration-300 group-hover:h-8 group-hover:w-8"
+			>
+				<img src="/svgs/white-star.svg" alt="" width={100} height={100} />
+			</div>
+			<span class="dot top-left"></span>
+			<span class="dot top-right"></span>
+			<span class="dot bottom-left"></span>
+			<span class="dot bottom-right"></span>
+		</div>
+	</div>
+</button>
--- src/lib/components/ui/header/Header.svelte
+++ src/lib/components/ui/header/Header.svelte
@@ -1,3 +1,8 @@
-<div class="fixed z-10 section-x-p h-40 w-full bg-red-500">
+<script lang="ts">
+	import Hamburger from "./Hamburger.svelte";
+</script>
+
+<div class="section-x-p section-y-p fixed z-10 flex w-full flex-row justify-between">
 	<img src="/logo/sklogo.svg" alt="logo" width="75" height="75" />
+	<Hamburger />
 </div>
